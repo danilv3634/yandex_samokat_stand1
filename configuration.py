8d23c72015a9f05ea76868aa65895c492dcd9e7c
danilv3634 <danil.vinogradov2020@mail.ru>
2024-09-29 09:28:49 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
-URL_SERVICE = "https://36531e55-d2a8-4157-8d28-68d61c4c80c7.serverhub.praktikum-services.ru"
-CREATE_ORDER_PATH="/api/v1/orders"
-GET_ORDER_TRACK = "/api/v1/orders/track?t={track}"
+URL_SERVICE = "https://7fdd7793-9aa4-4f5a-b749-11e36efa1ddd.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
